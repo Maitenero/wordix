75ed97984f232c6f1f328c2e95c3cfce9dc880a8
ezequiel95jr <ezequiel.retamal@est.fi.uncoma.edu.ar>
2022-11-02 20:45:29 -0300

    Estructura de datos 1

--- ProgramaAlmironCoronadoRetamal.php
+++ ProgramaAlmironCoronadoRetamal.php
@@ -30,7 +30,59 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
-/* ... COMPLETAR ... */
+/* ... COMPLETAR ... 
+$coleccionPartidas = [ $partida1,$partida2,$partida3,$partida4,$partida5,$partida6,$partida7,$partida8,$partida9,$partida10 ];
+
+$partida1["palabra"] = "MUJER";
+$partida1["nombre"] = "carlos";
+$partida1["puntaje"] = 0;
+$partida1["intento"] = 6; //verificar condicion, guardo 6 o guardo string de no gano
+
+$partida2["palabra"] = "GATOS";
+$partida2["nombre"] = "juan";
+$partida2["puntaje"] = 0;
+$partida2["intento"] = 6; //verificar condición
+
+$partida3["palabra"] = "CASAS";
+$partida3["nombre"] = "fernando";
+$partida3["puntaje"] = 0;
+$partida3["intento"] = 6; // verificarrrrrrr
+
+$partida4["palabra"] = "MELON";
+$partida4["nombre"] = "carlos";
+$partida4["puntaje"] = 0;
+$partida4["intento"] = 6; //verificar condición
+
+$partida5["palabra"] = "MELON";
+$partida5["nombre"] = "fernando";
+$partida5["puntaje"] = 11;
+$partida5["intento"] = 5; //verificar condición 
+
+$partida6["palabra"] = "MUJER";
+$partida6["nombre"] = "ezequiel";
+$partida6["puntaje"] = 0;
+$partida6["intento"] = 6; //verificar condición
+
+$partida7["palabra"] = "ANIMO";
+$partida7["nombre"] = "martin";
+$partida7["puntaje"] = 0;
+$partida7["intento"] = 6; //verificar condición
+
+$partida8["palabra"] = "BALAS";
+$partida8["nombre"] = "martin";
+$partida8["puntaje"] = 0;
+$partida8["intento"] = 6; //verificar condición
+
+$partida9["palabra"] = "BEBES";
+$partida9["nombre"] = "soho221";
+$partida9["puntaje"] = 0;
+$partida9["intento"] = 6; //verificar condición
+
+$partida10["palabra"] = "BALAS";
+$partida10["nombre"] = "abi";
+$partida10["puntaje"] = 0;
+$partida10["intento"] = 6; //verificar condición
+*/
 
 
 
